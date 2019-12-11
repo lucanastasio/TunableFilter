9f722062eba714a644f023ac89110dbf73218808
luca-anastasio <anastasio.lu@gmail.com>
2019-12-11 23:57:41 +0100

    gui

--- MCP4728_GUI/MCP4728.py
+++ MCP4728_GUI/MCP4728.py
@@ -2,28 +2,27 @@ from smbus2 import SMBus, i2c_msg
 
 
 class Channel:
-	PWR_ON = 0x0
-	PWR_DN_1K = 0x1
-	PWR_DN_100K = 0x2
-	PWR_DN_500K = 0x3
 	VREF_VDD = 0x0
 	VREF_INT = 0x1
 	GAIN_X1 = 0x0
 	GAIN_X2 = 0x1
-	UPD_OUT = 0x0
-	NO_UPD_OUT = 0x1
+	pwr = {
+		'POWER_ON': 0x0,
+		'DOWN_1K': 0x1,
+		'DOWN_100K': 0x2,
+		'DOWN_500K': 0x3
+	}
 
-	def __init__(self, channel, dac):
+	def __init__(self, chsel, dac):
 		self.voltage = 0.0
 		self.code = 0
 		self.codeChanged = False
 		self.step = 0.0005
-		self.channel = channel
+		self.chSel = chsel
 		self.vrefValue = 2.048
 		self.vrefSel = self.VREF_INT
 		self.gainSel = self.GAIN_X1
-		self.powerDownSel = self.PWR_ON
-		self.update = self.UPD_OUT
+		self.powerDownSel = self.pwr['POWER_ON']
 		self.vrefChanged = False
 		self.DAC = dac
 
@@ -35,14 +34,14 @@ class Channel:
 		self.step = 0.0005
 		self.voltage = self.code * self.step
 		if update:
-			self.DAC.Vref2048(self.channel)
+			self.DAC.Vref2048(self.chSel)
 
 	def Vref4096(self, update):
 		self.vrefValue = 4.096
 		self.step = 0.001
 		self.voltage = self.code * self.step
 		if update:
-			self.DAC.Vref4096(self.channel)
+			self.DAC.Vref4096(self.chSel)
 
 	def VrefVdd(self, vdd, update):
 
@@ -50,17 +49,23 @@ class Channel:
 		self.step = vdd / 4096
 		self.voltage = self.code * self.step
 		if update:
-			self.DAC.VrefVdd(self.channel)
+			self.DAC.VrefVdd(self.chSel)
 
 
 class MCP4728:
-	CMD_FAST_WR = 0x00
-	CMD_MULT_WR = 0x40
-	CMD_SING_WR = 0x58
-	CMD_SEQ_WR = 0x50
-	CMD_SEL_VREF = 0x80
-	CMD_SEL_GAIN = 0xC0
-	CMD_SEL_PWR = 0xA0
+	cmd = {
+		'FAST_WR': 0x00,
+		'MULT_WR': 0x40,
+		'SING_WR': 0x58,
+		'SEQ_WR': 0x50,
+		'SEL_VREF': 0x80,
+		'SEL_GAIN': 0xC0,
+		'SEL_PWR': 0xA0,
+		'GEN_RST': 0x06,
+		'GEN_WUP': 0x09,
+		'GEN_UPD': 0x08,
+		'GEN_ADD': 0x0C
+	}
 
 	def __init__(self, bus, address):
 		self.address = address
@@ -70,34 +75,134 @@ class MCP4728:
 						'C': Channel(0x2, self),
 						'D': Channel(0x3, self)}
 
-	def fastwrite(self):
+	def FastWrite(self):
 		"""
-		Writes all 4 channels' input data and power down registers
+		Writes all 4 channels' input data and power down registers (EEPROM not affected)
 		NOTE: Doesn't update Vout (output data registers)
 		"""
 		data = []
-		for ch in self.channel.values():
+		for c in 'ABCD':
+			ch = self.channel[c]
 			code = ch.code.to_bytes(2, 'big')
-			data.append(self.CMD_FAST_WR | (ch.powerDownSel << 4) | code[0])
+			data.append(self.cmd['FAST_WR'] | (ch.powerDownSel << 4) | code[0])
 			data.append(code[1])
 		msg = i2c_msg.write(self.address, data)
 		self.i2c.i2c_rdwr(msg)
 
-	def multiwrite(self, channel):
+	def MultiWrite(self, channels, update=True):
 		"""
-		Writes the selected channel(s) register(s)
+		Writes the selected channel(s) register(s) (EEPROM not affected)
 		(including: data input register, gain, power down, Vref)
 		Also determines if Vout gets updated or not
 
-		:param channel: the channel(s) to write
-		:type channel: str
+		:param channels: the channel(s) to write (e.g. 'ABCD' or 'ABC' or 'CD' or 'B' and so on)
+		:type channels: str
+		:param update: trigger an update of Vout if True, else just write register
+		:type update: bool
 		"""
 		data = []
-		for c in channel:
+		for c in channels:
 			ch = self.channel[c]
 			code = ch.code.to_bytes(2, 'big')
-			data.append(self.CMD_MULT_WR | (ch.channel << 1) | ch.update)
+			data.append(self.cmd['MULT_WR'] | (ch.chSel << 1) | int(not update))
 			data.append((ch.vrefSel << 7) | (ch.powerDownSel << 5) | (ch.gainSel << 4) | code[0])
 			data.append(code[1])
 		msg = i2c_msg.write(self.address, data)
 		self.i2c.i2c_rdwr(msg)
+
+	def SequentialWrite(self, start, update=True):
+		"""
+		Writes registers and EEPROM starting from the given channel to channel D
+
+		:param start: the starting channel (e.g. 'A' or 'B' or 'C' or 'D')
+		:type start: str
+		:param update: trigger an update of Vout if True, else just write registers and EEPROM
+		:type update: bool
+		"""
+		data = [(self.cmd['SEQ_WR'] | self.channel[start].chSel << 1 | int(not update))]
+		for c in range(ord(start), ord('D')+1):
+			ch = self.channel[chr(c)]
+			code = ch.code.to_bytes(2, 'big')
+			data.append((ch.vrefSel << 7) | (ch.powerDownSel << 5) | (ch.gainSel << 4) | code[0])
+			data.append(code[1])
+		msg = i2c_msg.write(self.address, data)
+		self.i2c.i2c_rdwr(msg)
+
+	def SingleWrite(self, channel, update=True):
+		"""
+		Writes registers and EEPROM only for the given channel
+
+		:param channel: channel to write (e.g. 'A' or 'B' or 'C' or 'D')
+		:type channel: str
+		:param update: trigger an update of Vout if True, else just write registers and EEPROM
+		:type update: bool
+		"""
+		ch = self.channel[channel]
+		data = [(self.cmd['SING_WR'] | ch.chSel << 1 | int(not update))]
+		code = ch.code.to_bytes(2, 'big')
+		data.append((ch.vrefSel << 7) | (ch.powerDownSel << 5) | (ch.gainSel << 4) | code[0])
+		data.append(code[1])
+		msg = i2c_msg.write(self.address, data)
+		self.i2c.i2c_rdwr(msg)
+
+	def WriteAddress(self, newaddr):
+		"""
+		Unimplemented, requires dedicated pin
+		"""
+		pass
+
+	def WriteVref(self):
+		"""
+		Writes Vref selection bits, does not affect EEPROM
+		"""
+		pass
+
+	def WriteGain(self):
+		"""
+		Writes Gain selection bits, does not affect EEPROM
+		"""
+		pass
+
+	def WritePower(self):
+		"""
+		Writes Power selection bits, does not affect EEPROM
+		"""
+		pass
+
+	def ReadAll(self):
+		"""
+		Reads registers and EEPROM contents and saves values in current state
+		"""
+		msg = i2c_msg.read(self.address, 24)
+		# in case it doesn't work, try to split in messages of 3 bytes each
+		self.i2c.i2c_rdwr(msg)
+		#msg.
+		pass
+
+	@staticmethod
+	def GeneralCallReset():
+		"""
+
+		"""
+		pass
+
+	@staticmethod
+	def GeneralCallWakeUp():
+		"""
+
+		"""
+		pass
+
+	@staticmethod
+	def GeneralCallSoftwareUpdate():
+		"""
+
+		"""
+		pass
+
+	@staticmethod
+	def GeneralCallReadAddress():
+		"""
+
+		"""
+		pass
